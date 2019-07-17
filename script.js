78b178979d4468cd4eadd9b08f7ddfd27989fee9
KeeneWong <keenewonghofung@gmail.com>
2019-07-16 20:54:01 -0400

    add alert when player guess the right word

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-let wordToGuess = "";
+let wordToGuess = "a";
+const getplayStage = document.querySelector('.playStage');
+const getinputStage = document.querySelector('.inputStage');
 const getGuessBtn = document.querySelector('.guess');
+const getinput = document.querySelector('.input');
+const getuserinput = document.querySelector('.userinput');
+const getguessArea = document.querySelector('#guessArea');
 
+
+//create the word to guess and the blankline--------------------
 getGuessBtn.addEventListener('click',function(){
     let blankLine = [];
-    wordToGuess = document.querySelector('.input').value;
+    wordToGuess = getinput.value;
     // console.log(wordToGuess);
     wordToGuessArray = wordToGuess.split("");
     wordToGuessArray.forEach(letter=>{
@@ -11,7 +18,55 @@ getGuessBtn.addEventListener('click',function(){
     });
     // console.log(blankLine);
     
-    document.getElementById('guessArea').innerText = '';
+    // To get the ID guessArea and put the blank line in it
+    document.getElementById('guessArea').innerText = blankLine.join().replace(/,/g,"");
+
+    getplayStage.classList.toggle('hidden');
+    getinputStage.classList.toggle('hidden');
 
 })
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// keyboard function and guess----------------------------------------
+
+const keyboards = document.querySelectorAll('.letter');
+
+keyboards.forEach(function(key){
+    key.addEventListener('click',function(event){
+        
+    let typeletter = key.getAttribute('data-letter');
+    console.log(typeletter);
+    let guessAreaArray = getguessArea.innerText.split('');
+    // console.log(guessAreaArray)
+
+
+    if(wordToGuess.includes(typeletter)===true){
+    guessAreaArray[wordToGuess.indexOf(typeletter)] = typeletter;
+    getguessArea.innerText = guessAreaArray.join().replace(/,/g,"");
+    
+    console.log(guessAreaArray);
+    if(getguessArea.innerText==wordToGuess){
+        alert(`You got the right word!!!`)
+    }
+    }
+    })
+
+    // if(getguessArea.innerText===wordToGuess){
+    //     alert(`That's the right word!!`)
+    // }
+})
